fdf49113ebd7ddc84361ff0a7bcb3f03384816ae
Benjamin Zaporzan <btzaporz@lakeheadu.ca>
2015-10-15 22:53:58 -0400

    updated cljs(build) versions

--- project.clj
+++ project.clj
@@ -4,9 +4,9 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/clojurescript "0.0-3308"]
+                 [org.clojure/clojurescript "1.7.145"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]]
-  :plugins [[lein-cljsbuild "1.0.6"]]
+  :plugins [[lein-cljsbuild "1.1.0"]]
   :cljsbuild 
   {:builds [{:source-paths ["src/cljs"]
              :compiler {:output-to "resources/public/js/schutzen.js" 
